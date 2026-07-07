9a68fe91374384395f63cc450bcee77f8dd4122d
rahini130 <rahimem2407@gmail.com>
2026-07-07 18:28:20 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/SwiftCheckOut/security/SecurityConfig.java
+++ src/main/java/com/example/SwiftCheckOut/security/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration=new CorsConfiguration();
                configuration.setAllowedOrigins(List.of("http://localhost:5500","http://127.0.0.1:5500","http://localhost:5501",
-            "http://127.0.0.1:5501","http://127.0.0.1:5502"
+            "http://127.0.0.1:5501","http://127.0.0.1:5502","https://rahini130.github.io/"
         ));
 
         configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
